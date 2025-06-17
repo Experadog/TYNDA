e0eeafdca62db6d24fabc29e067443b36af41768
Aidin <omurkulovaidin94@gmail.com>
2025-06-18 00:34:03 +0600

    test 6

--- src/common/custom-errors/global-error.tsx
+++ src/common/custom-errors/global-error.tsx
@@ -4,6 +4,7 @@ import { URL_LOCAL_ENTITIES } from '@/lib';
 import { Button, ImgMask, LoadingSpinner } from '@components';
 import { AlertTriangle } from 'lucide-react';
 import React, { type ReactNode } from 'react';
+import { sendErrorToTelegram } from '../actions/sendUserErrorToTelegram';
 
 type Props = {
 	children: ReactNode;
@@ -34,6 +35,8 @@ class ErrorBoundary extends React.Component<Props, State> {
 	static getDerivedStateFromError(error: Error): State {
 		const msg = error.message?.toLowerCase() || '';
 		const isSessionExpired = msg.includes('401') || msg.includes('unauthorized');
+
+		sendErrorToTelegram({ message: JSON.stringify(error) });
 
 		return {
 			hasError: true,
